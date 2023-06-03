3633616f0aedc20198b95a2678a434f1ea47747d
JeikZim <meatcatcher141@gmail.com>
2023-06-03 23:58:13 +0300

    03.06.23 pre-save

---
+++ middlewares/auth.admin.middleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = (req, res, next) => {
+    if (req.method == "OPTIONS") return next();
+
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        let isExpire = false;
+
+        if (!token) {
+            return res.status(401).json({ message: "User not authorized" });
+        }
+
+        jwt.verify(token, config.get("jwtSecret"), (err, decoded) => {
+            if (err) {
+                isExpire = err.message === 'jwt expired';
+            } else {
+                req.admin = decoded;
+            }
+        });
+        if (isExpire) {
+            return res.status(200).json({ isExpire });
+        } 
+
+        next();
+    } catch (err) {
+        
+    }
+}
